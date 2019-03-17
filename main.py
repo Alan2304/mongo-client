27c4d4a2349168dea738062b4e4108ac10fe3956
Alan2304 <sanchezalan95@gmail.com>
2019-03-17 17:49:43 -0600

    Added console interface to interact with the functions

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from mongoConfig import *
+from Posts import *
 def main():
-    db = connectMongo()
-    testCollection = getCollection('test-collection', db)
-    post = {
-        "author": "Alan",
-        "text": "Hello I'm Alan"
-    }
-    post_id = testCollection.insert_one(post).inserted_id
-    print (post_id)
+    while 1:
+        print("1.- Create Post\n2.-Update Post\n3.-Delete Post\n4.-Read Posts\n5.-Find by tag\n6.-Exit")
+        option = input("Enter the option: ")
+        if option == "6":
+            print("Exit Program")
+            exit()
+        
+        switcher = {
+            "1": Posts.insert,
+            "2": Posts.update,
+            "3": Posts.delete,
+            "4": Posts.read,
+            "5": Posts.findByTag
+        }
+        print(option)
+        operation = switcher.get(option, lambda: "Invalid operation")
+        operation()
 
 main()
